8a64563123df00726f103a184b6e56e4380dc5bf
Sh1yden <pvasa8917@gmail.com>
2025-09-17 05:19:20 +0300

    split files, add start, main func

---
+++ __main__.py
@@ -0,0 +1,50 @@
+import asyncio
+import logging
+import json
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from bot.handlers.start import router as start_router
+
+
+def get_token():
+
+    with open(
+        "c:\\Users\\Shayden\\Documents\\All Code Programming\\GitHub\\_secret_api_keys\\api_keys.json",
+        "r",
+        encoding="utf-8",
+    ) as f:
+        data = json.load(f)
+        return data["AIO_BOT_TOKEN"]
+
+
+def create_bot() -> Bot:
+
+    bot = Bot(
+        token=get_token(), default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+
+    return bot
+
+
+def create_dispatcher() -> Dispatcher:
+
+    dp = Dispatcher()
+
+    dp.include_router(start_router)
+
+    return dp
+
+
+async def main():
+
+    bot = create_bot()
+    dp = create_dispatcher()
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
